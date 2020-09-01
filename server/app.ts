393090ca3b61418eac3def3a47d021854d2f92b8
Robert, Drew <ddrobe2@ilstu.edu>
2020-09-01 18:32:34 -0500

    add cors config

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import { indexRouter } from "./routes/index";
+import { indexRouter } from "./routes";
 import { covidRouter } from "./routes/covid";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/covid", covidRouter);
