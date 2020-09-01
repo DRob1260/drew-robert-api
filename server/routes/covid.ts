9f331c5d144c1004d73df90e418bdae6fc7350c2
Robert, Drew <ddrobe2@ilstu.edu>
2020-08-31 22:36:18 -0500

    adding endpoint for statewide data

--- server/routes/covid.ts
+++ server/routes/covid.ts
@@ -1,8 +1,37 @@
 import express, { Request, Response } from "express";
 import { getIllinoisCovidData } from "../delegators/covid/illinois";
-import { processIllinoisCovidData } from "../processors/covid/illinois";
+import {
+  processIllinoisCountyCovidData,
+  processIllinoisCovidData,
+} from "../processors/covid/illinois";
 
 const covidRouter = express.Router();
+
+covidRouter.get(
+  "/country/:country/state/:state",
+  (req: Request, res: Response) => {
+    const country = req.params.country.toLowerCase();
+    const state = req.params.state.toLowerCase();
+
+    if (country === "usa" && state === "il") {
+      getIllinoisCovidData()
+        .then((data) => {
+          const historicalRecord = processIllinoisCovidData(data);
+          res.send(historicalRecord);
+        })
+        .catch((error) => {
+          console.log(
+            `error while retrieving illinois covid data: ${error.message}`
+          );
+          res.status(500);
+          res.send();
+        });
+    } else {
+      res.status(404);
+      res.send();
+    }
+  }
+);
 
 covidRouter.get(
   "/country/:country/state/:state/county/:county",
@@ -12,10 +41,21 @@ covidRouter.get(
     const county = req.params.county.toLowerCase();
 
     if (country === "usa" && state === "il" && county === "mclean") {
-      getIllinoisCovidData().then((data) => {
-        const historicalCountyRecords = processIllinoisCovidData(data, county);
-        res.send(historicalCountyRecords);
-      });
+      getIllinoisCovidData()
+        .then((data) => {
+          const historicalRecords = processIllinoisCountyCovidData(
+            data,
+            county
+          );
+          res.send(historicalRecords);
+        })
+        .catch((error) => {
+          console.log(
+            `error while retrieving illinois county ${county} covid data: ${error.message}`
+          );
+          res.status(500);
+          res.send();
+        });
     } else {
       res.status(404);
       res.send();
