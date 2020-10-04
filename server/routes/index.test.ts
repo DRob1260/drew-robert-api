77d55fac8ff1be8a9d9febd51548a0e17c54c285
Robert, Drew <ddrobe2@ilstu.edu>
2020-10-03 21:29:38 -0500

    remove test that is a false positive

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -1,32 +1,10 @@
-import { setupServer } from "msw/node";
-import { rest } from "msw";
-import { Urls } from "../config";
 import request from "supertest";
 import app from "../../test/app";
 import { indexRouter } from "./index";
 
-const server = setupServer(
-  rest.get(`${Urls.drewRobertSite}`, (req, res, ctx) => {
-    return res(ctx.status(200));
-  })
-);
-
 app.use("/", indexRouter);
 
-test("GET /", async (done) => {
-  server.listen();
-  request(app)
-    .get("/")
-    .then((request) => {
-      expect(request.status).toEqual(200);
-    })
-    .then(() => {
-      server.close();
-      done();
-    });
-});
-
-test("GET /covid", async (done) => {
+test("GET /covid-api", async (done) => {
   request(app)
     .get("/covid-api")
     .then((request) => {
