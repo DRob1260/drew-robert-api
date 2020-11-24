84e006db391c89e0d0811a3e128308c89c5c00f1
Robert, Drew <ddrobe2@ilstu.edu>
2020-11-23 19:48:13 -0600

    no longer using drew-robert-api as proxy

--- server/app.ts
+++ server/app.ts
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(`/`, indexRouter);
+app.use(`/api`, indexRouter);
 
 export default app;
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,37 +1,8 @@
 import express from "express";
 import { covidRouter } from "./covid/covidRouter";
-import { createProxyMiddleware } from "http-proxy-middleware";
-import { Urls } from "../config";
 
 const indexRouter = express.Router();
 
-indexRouter.use("/covid-api", covidRouter);
-
-indexRouter.use(
-  "/redcycle-ui",
-  createProxyMiddleware(Urls.redcycleUi, {
-    protocolRewrite: "https",
-    changeOrigin: true,
-    target: Urls.redcycleUi,
-  })
-);
-
-indexRouter.use(
-  "/redcycle-api/*",
-  createProxyMiddleware(Urls.redcycleApi, {
-    protocolRewrite: "https",
-    changeOrigin: true,
-    target: Urls.redcycleApi,
-  })
-);
-
-indexRouter.all(
-  "/*",
-  createProxyMiddleware(Urls.drewRobertSite, {
-    protocolRewrite: "https",
-    changeOrigin: true,
-    target: Urls.drewRobertSite,
-  })
-);
+indexRouter.use("/covid", covidRouter);
 
 export { indexRouter };
