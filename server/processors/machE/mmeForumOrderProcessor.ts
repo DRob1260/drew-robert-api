35e6ffb5be6527a636d912a2eb251d824707bd9a
Robert, Drew <ddrobe2@ilstu.edu>
2021-08-21 17:10:46 -0500

    migrate to Google Sheets API v4

--- server/processors/machE/mmeForumOrderProcessor.ts
+++ server/processors/machE/mmeForumOrderProcessor.ts
@@ -1,135 +1,55 @@
 import { MmeForumOrder } from "../../models/machE/MmeForumOrder";
-import axios, { AxiosResponse } from "axios";
-import {
-  EntryEntity,
-  Sheet,
-} from "../../models/GoogleSheets/GoogleSheetsInterfaces";
+import { google, sheets_v4 } from "googleapis";
+import { EntryEntity } from "../../models/GoogleSheets/GoogleSheetsInterfaces";
 
 export const getMmeForumOrders = (): Promise<Array<MmeForumOrder>> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      axios
-        .get(
-          "https://spreadsheets.google.com/feeds/cells/1lBgAEQVufjImIIq3wllYvqupRNTZD3wfSoiRZRrJlAg/1/public/full?alt=json"
-        )
-        .then((axiosResponse: AxiosResponse<Sheet>) => {
-          const mmeForumOrders: MmeForumOrder[] = generateMmeForumOrders(
-            axiosResponse.data.feed.entry
-          );
-          resolve(mmeForumOrders);
-        });
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
-
-const generateMmeForumOrders = (sheetEntries: Array<EntryEntity>) => {
-  const mmeForumOrders: MmeForumOrder[] = [];
-
-  const rows = generateSheetRows(sheetEntries);
-
-  rows.forEach((rowEntries) => {
-    mmeForumOrders.push(mapRowEntriesToOrder(rowEntries));
+  const sheets = google.sheets("v4");
+  const auth = new google.auth.GoogleAuth({
+    scopes: [
+      "https://www.googleapis.com/auth/spreadsheets",
+      "https://www.googleapis.com/auth/spreadsheets.readonly",
+    ],
   });
 
-  return mmeForumOrders;
-};
+  return auth.getClient().then((authClient) => {
+    google.options({ auth: authClient });
 
-const generateSheetRows = (
-  sheetEntries: Array<EntryEntity>
-): Map<number, EntryEntity[]> => {
-  const rowMap: Map<number, EntryEntity[]> = new Map<number, EntryEntity[]>();
+    const request: sheets_v4.Params$Resource$Spreadsheets$Values$Get = {
+      spreadsheetId: "1lBgAEQVufjImIIq3wllYvqupRNTZD3wfSoiRZRrJlAg",
+      range: "B7:V100000",
+    };
 
-  sheetEntries.forEach((entry) => {
-    if (parseInt(entry.gs$cell.row) >= 7) {
-      const rowNumber = parseInt(entry.gs$cell.row);
-      const rowEntries = rowMap.get(rowNumber) || [];
-      rowEntries.push(entry);
-      rowMap.set(rowNumber, rowEntries);
-    }
+    return sheets.spreadsheets.values.get(request).then((response) => {
+      const mmeForumOrders: MmeForumOrder[] = [];
+
+      response.data.values.forEach((row) => {
+        if (row) {
+          mmeForumOrders.push({
+            orderEntryNumber: parseInt(row[0]),
+            mmeForumUsername: row[1],
+            orderNumber: row[2],
+            orderDate: row[3],
+            estimatedBuildDate: row[4],
+            actualBuildDate: row[5],
+            estimatedDeliveryDate: row[6],
+            actualDeliveryDate: row[7],
+            vin: row[8],
+            vinReceived: row[9].toLocaleLowerCase() === "yes",
+            daysBetweenOrderAndBuild: parseInt(row[10]),
+            daysBetweenOrderAndDelivery: parseInt(row[11]),
+            daysBetweenBuildAndDelivery: parseInt(row[12]),
+            location: row[13],
+            locationNonUS: row[14],
+            exteriorColor: row[15],
+            modelYear: row[16],
+            model: row[17],
+            driveTrain: row[18],
+            battery: row[19],
+          });
+        }
+      });
+
+      return mmeForumOrders;
+    });
   });
-
-  return rowMap;
-};
-
-const mapRowEntriesToOrder = (rowEntries: EntryEntity[]): MmeForumOrder => {
-  // @ts-ignore
-  let mmeForumOrder: MmeForumOrder = {};
-
-  rowEntries.forEach((entry) => {
-    switch (entry.gs$cell.col) {
-      case "2":
-        mmeForumOrder.orderEntryNumber = parseInt(entry.gs$cell.inputValue);
-        break;
-      case "3":
-        mmeForumOrder.mmeForumUsername = entry.gs$cell.inputValue;
-        break;
-      case "4":
-        mmeForumOrder.orderNumber = entry.gs$cell.inputValue;
-        break;
-      case "5":
-        mmeForumOrder.orderDate = entry.gs$cell.inputValue;
-        break;
-      case "7":
-        mmeForumOrder.estimatedBuildDate = entry.gs$cell.inputValue;
-        break;
-      case "8":
-        mmeForumOrder.actualBuildDate = entry.gs$cell.inputValue;
-        break;
-      case "9":
-        mmeForumOrder.estimatedDeliveryDate = entry.gs$cell.inputValue;
-        break;
-      case "10":
-        mmeForumOrder.actualDeliveryDate = entry.gs$cell.inputValue;
-        break;
-      case "11":
-        mmeForumOrder.vin = entry.gs$cell.inputValue;
-        break;
-      case "12":
-        mmeForumOrder.vinReceived =
-          entry.gs$cell.inputValue.toLocaleLowerCase() === "yes";
-        break;
-      case "13":
-        mmeForumOrder.daysBetweenOrderAndBuild = parseInt(
-          entry.gs$cell.inputValue
-        );
-        break;
-      case "14":
-        mmeForumOrder.daysBetweenOrderAndDelivery = parseInt(
-          entry.gs$cell.inputValue
-        );
-        break;
-      case "15":
-        mmeForumOrder.daysBetweenBuildAndDelivery = parseInt(
-          entry.gs$cell.inputValue
-        );
-        break;
-      case "16":
-        mmeForumOrder.location = entry.gs$cell.inputValue;
-        break;
-      case "17":
-        mmeForumOrder.locationNonUS = entry.gs$cell.inputValue;
-        break;
-      case "18":
-        mmeForumOrder.exteriorColor = entry.gs$cell.inputValue;
-        break;
-      case "19":
-        mmeForumOrder.modelYear = entry.gs$cell.inputValue;
-        break;
-      case "20":
-        mmeForumOrder.model = entry.gs$cell.inputValue;
-        break;
-      case "21":
-        mmeForumOrder.driveTrain = entry.gs$cell.inputValue;
-        break;
-      case "22":
-        mmeForumOrder.battery = entry.gs$cell.inputValue;
-        break;
-      default:
-        break;
-    }
-  });
-
-  return mmeForumOrder;
 };
